7defbcef2f9600222fe817742fa1d0a96d3c25af
Piero Rendina <79834739+PieroRendina@users.noreply.github.com>
2022-06-07 08:38:46 +0200

    updated run.sh script

--- cuda_CONTEST/run.sh
+++ cuda_CONTEST/run.sh
@@ -1,9 +1,12 @@
 #!/bin/bash
 
 # Run vector sum;
-bin/b -d -c -n 100000000 -b vec -I 1 -i 30 -t 64;
-bin/b -d -c -n 100000000 -b vec -I 2 -i 30 -t 64;
+# bin/b -d -c -n 100000000 -b vec -I 1 -i 30 -t 64;
+# bin/b -d -c -n 100000000 -b vec -I 2 -i 30 -t 64;
 
 # Run matrix multiplication;
-bin/b -d -c -n 1000 -b mmul -I 1 -i 30 -t 8;
-bin/b -d -c -n 1000 -b mmul -I 2 -i 30 -t 8 -B 14;
+# bin/b -d -c -n 1000 -b mmul -I 1 -i 30 -t 8;
+# bin/b -d -c -n 1000 -b mmul -I 2 -i 30 -t 8 -B 14;
+
+# Run personalized page rank;
+bin/b -d -n 1000 -b ppr -I 1 -i 1 -t 32 -B 64
